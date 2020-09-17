a90b3381b85ed31666e4ad3a0b3692d2ebcfe666
Re7z0r <33451953+Re7z0r@users.noreply.github.com>
2020-09-17 14:18:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,72 @@ from sly import Lexer
 
 class LexerAnalyzer(Lexer):
     # Set of token names. This is always required 
-    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, LPAREN, RPAREN, SCOLON,
-               LBRACK, RBRACK, LBRACE, RBRACE, COLON,
-               EQUAL, AND, LESS, DOT, NOT }
+    tokens = { ID, AND, ASSIGN, COLON, DIVIDE, DOT, EQUAL, LBRACE, LBRACK, LESS, 
+        LPAREN, MINUS, NOT, NUMBER, PLUS, RBRACK, RBRACE, RPAREN, SCOLON, TIMES, 
+        BOOLEAN, CLASS, ELSE, EXTENDS, FALSE, IF, INT, LENGTH, MAIN, NEW, MAIN, 
+        NEW, PUBLIC, RETURN, STATIC, STRING, WRITE, THIS, TRUE, VOID, WHILE, WRITE }
     
     # String containing ignored characters between tokens
-    ignore = ' \t\n'
-    
-    # Regular expression rules for tokens
+    ignore = r' \t\n'    
+    ignore_comment = r'\#.*'
+
+    # Regular expression rules for tokens in alphabetical order
     ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    AND     = r'&&'
+    ASSIGN  = r'='
+    COLON   = r','
+    DIVIDE  = r'/'
+    DOT     = r'\.'
+    EQUAL   = r'='
+    LBRACE  = r'\{'  
+    LBRACK  = r'\['
+    LESS    = r'<'
+    LPAREN  = r'\('
+    MINUS   = r'-'
+    NOT     = r'!'
     NUMBER  = r'\d+'
     PLUS    = r'\+'
-    MINUS   = r'-'
-    TIMES   = r'\*'
-    DIVIDE  = r'/'
-    ASSIGN  = r'='
-    LPAREN  = r'\('
+    RBRACK  = r'\]'
+    RBRACE  = r'\}'
     RPAREN  = r'\)'
-    SCOLON  = r';'
-    
-    LBRACK = r'\['
-    RBRACK = r'\]'
-    LBRACE = r'\{'
-    RBRACE = r'\}'
-    COLON = r','
-    EQUAL = r'='
-    AND = r'&&'
-    LESS = r'<'
-    DOT = r'.'
-    NOT = r'!'
+    SCOLON  = r';'  
+    TIMES   = r'\*'
+
+    # Words reserved in alphabetical order by type
+    ID['boolean']   = BOOLEAN
+    ID['class']     = CLASS
+    ID['else']      = ELSE
+    ID['extends']   = EXTENDS
+    ID['false']     = FALSE
+    ID['if']        = IF
+    ID['int']       = INT
+    ID['length']    = LENGTH
+    ID['main']      = MAIN
+    ID['new']       = NEW
+    ID['public']    = PUBLIC
+    ID['return']    = RETURN
+    ID['static']    = STATIC
+    ID['string']    = STRING
+    ID['this']      = THIS
+    ID['true']      = TRUE
+    ID['void']      = VOID
+    ID['while']     = WHILE
+    ID['System.out.println']    = WRITE
+
+    # Line number tracking
+    @_(r'\n+')
+    def ignore_newline(self, t):
+        self.lineno += t.value.count('\n')
+
 
 if __name__ == '__main__':
 #    data = input('lex > ') 
-    arquivo = open('data.in', 'r')
-    data = arquivo.read()
+    # open archive named data.in in read mode
+    archive = open('data.in', 'r')
+    data = archive.read()
     print(data)
 
     lexer = LexerAnalyzer()
-    for tok in lexer.tokenize(data):
-        print('type=%r, value=%r lineno=%r index=%r' % (tok.type, tok.value, tok.lineno, tok.index))
-    arquivo.close()
+    for tok in lexer.tokenize(data):     
+        print('type=%r, value=%r lineno=%r' % (tok.type, tok.value, tok.lineno))
+    archive.close()
