78795b33d1ba022efb8257e2edef25bf138b8c79
re7z0r <takajp.3387@gmail.com>
2020-09-17 21:16:32 -0400

    System.out.println adicionado como token

--- main.py
+++ main.py
@@ -3,27 +3,21 @@ from sly import Lexer
 class LexerAnalyzer(Lexer):
     # Set of token names. This is always required 
     tokens = { ID, AND, ASSIGN, COLON, DIVIDE, DOT, EQUAL, LBRACE, LBRACK, 
-        LESS, LPAREN, MINUS, NOT, NUMBER, PLUS, RBRACK, RBRACE, 
-        RPAREN, SCOLON, TIMES, 
-        BOOLEAN, CLASS, ELSE, EXTENDS, FALSE, IF, INT, LENGTH, MAIN, NEW, MAIN, 
-        NEW, PUBLIC, RETURN, STATIC, STRING, WRITE, THIS, TRUE, VOID, WHILE, WRITE }
+        LESS, LPAREN, MINUS, NOT, NUMBER, PLUS, RBRACK, RBRACE, RPAREN, SCOLON, TIMES, 
+        BOOLEAN, CLASS, ELSE, EXTENDS, FALSE, IF, INT, LENGTH, MAIN, NEW, 
+        PUBLIC, RETURN, STATIC, STRING, WRITE, THIS, TRUE, VOID, WHILE, WRITE }
 
-    # COMMENT, LCOMMENTBLOCK, RCOMMENTBLOCK,
-    
     # String containing ignored characters between tokens
     ignore = r' \t\n'    
     ignore_comment = r'\/\/.*'
     ignore_block_comment = r'\/\*[^\*\/]+\*\/'
 
     # Regular expression rules for tokens in Priority order
-    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID      = r'[a-zA-Z_][a-zA-Z0-9_.]*'
     NUMBER  = r'\d+'
     DOT     = r'\.'
     COLON   = r','
-    SCOLON  = r';'  
-    # COMMENT = r'\/\/'       # //
-    # LCOMMENTBLOCK = r'\/\*' # /*
-    # RCOMMENTBLOCK = r'\*\/' # *\
+    SCOLON  = r';'
     LBRACE  = r'\{'  
     RBRACE  = r'\}'
     LBRACK  = r'\['
@@ -40,7 +34,7 @@ class LexerAnalyzer(Lexer):
     LESS    = r'<'
     AND     = r'&&'
 
-    # Words reserved in Alphabetical order by type
+    # Words reserved
     ID['class']     = CLASS
     ID['public']    = PUBLIC
     ID['static']    = STATIC
@@ -68,6 +62,7 @@ class LexerAnalyzer(Lexer):
 
     ID['System.out.println']    = WRITE
 
+
     # Line number tracking
     @_(r'\n+')
     def ignore_newline(self, t):
@@ -76,12 +71,12 @@ class LexerAnalyzer(Lexer):
 
 if __name__ == '__main__':
 #    data = input('lex > ') 
-    # open archive named data.in in read mode
-    archive = open('data.in', 'r')
-    data = archive.read()
-    # print(data)
+    # open file named data.in in read mode
+    file = open('data.in', 'r')
+    data = file.read()
+    print(data)
 
     lexer = LexerAnalyzer()
     for tok in lexer.tokenize(data):     
         print('type=%r, value=%r lineno=%r index=%r' % (tok.type, tok.value, tok.lineno, tok.index))
-    archive.close()
+    file.close()
